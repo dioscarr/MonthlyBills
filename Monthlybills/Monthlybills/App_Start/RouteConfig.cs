c67e6586efdb37d421afbf53683fcf9dcf8c8a63
dioscarr <Dioscard@hotmail.com>
2015-02-15 23:28:04 -0500

    ---change default route to login--

--- Monthlybills/Monthlybills/App_Start/RouteConfig.cs
+++ Monthlybills/Monthlybills/App_Start/RouteConfig.cs
@@ -17,7 +17,7 @@ namespace Monthlybills
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
